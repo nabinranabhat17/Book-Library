5d0711c088529271ee279cc4f0f274e7ca1ffd87
FlamboyantFate <105473396+FlamboyantFate@users.noreply.github.com>
2024-10-05 16:57:05 +0545

    https

--- src/App.js
+++ src/App.js
@@ -1,52 +1,49 @@
+``;
+import { useEffect, useState } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-import { useEffect, useState } from 'react';
-import './App.css';
-import searchIcon from './search.svg';
-import MovieCard from './MovieCard';
-
-const apiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=826dc2cd';
+const apiUrl = "https://www.omdbapi.com/?i=tt3896198&apikey=826dc2cd";
 
 function App() {
-  const [search, setSearch] = useState('Spiderman');
-  const [searchname, setSearchname] = useState('');
+  const [search, setSearch] = useState("Spiderman");
+  const [searchname, setSearchname] = useState("");
   const [movies, setMovies] = useState([]);
 
-  const searchHandle = async (title) =>{
+  const searchHandle = async (title) => {
     const response = await fetch(`${apiUrl}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
-  }
+  };
   useEffect(() => {
-    searchHandle(search); 
+    searchHandle(search);
   }, [searchname]);
-  
-  function searchName(e){
+
+  function searchName(e) {
     setSearch(e.target.value);
   }
 
-  function searchActivate(){
-    if (search == ''){
-      setSearch('Spiderman');
+  function searchActivate() {
+    if (search == "") {
+      setSearch("Spiderman");
     }
     setSearchname(search);
   }
   return (
     <>
-    <div className='app'>
-      <h1>Movie App</h1>
-      <div className='search'>
-        <input type="text" value={search} onChange={searchName}/>
-        <img src={searchIcon} alt="search" onClick={searchActivate}/>
-      </div>
-      <div className="empty">
-        {movies.map((movie)=>{
-          return(
-            <MovieCard movie={movie}/>
-          )})}
+      <div className="app">
+        <h1>Movie App</h1>
+        <div className="search">
+          <input type="text" value={search} onChange={searchName} />
+          <img src={searchIcon} alt="search" onClick={searchActivate} />
+        </div>
+        <div className="empty">
+          {movies.map((movie) => {
+            return <MovieCard movie={movie} />;
+          })}
         </div>
       </div>
-        
-      
     </>
   );
 }
